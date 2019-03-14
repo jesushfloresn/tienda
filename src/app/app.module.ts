ac010a9dfc65d291e6312cdb48c90af7135bba5f
jesushfloresn <jesushfloresn@gmail.com>
2019-03-13 23:34:00 -0600

    add project repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from 'angularfire2';
+import { environment } from '../environments/environment';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDImZh8_0u875-0U6FcPlG5Jh4l4lGTBjs",
+    authDomain: "angular5-6d4c2.firebaseapp.com",
+    databaseURL: "https://angular5-6d4c2.firebaseio.com",
+    projectId: "angular5-6d4c2",
+    storageBucket: "angular5-6d4c2.appspot.com",
+    messagingSenderId: "145219211636"
+  }
 };
